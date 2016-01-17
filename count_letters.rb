9574ce2e9346b2a830520e904c77af2edf1b76d0
Frank <mail@frankruegamer.de>
2016-01-17 01:05:12 +0100

    Insert newline

--- count_letters.rb
+++ count_letters.rb
@@ -2,7 +2,8 @@ def count_letters(text)
   count = Hash.new(0)
   text.chars.each { |c| count[c.downcase] += 1 if c =~ /[a-zA-Z]/ }
   count = count.sort_by { |_k, v| -v }
-  puts; count.each { |k, v| puts "#{k}: #{v}" }
+  puts
+  count.each { |k, v| puts "#{k}: #{v}" }
 end
 
 ARGV.each { |arg| count_letters(arg) }
